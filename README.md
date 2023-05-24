2fd594f01c499f3569d6e3b7a0ecee6c2e6abea3
zwj <742188034@qq.com>
2023-05-24 16:29:06 +0800

    1、新增glide

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Mode2
+# 新增沉浸式状态栏 ImmersionBar 
+# GitHUb地址:https://github.com/gyf-dev/ImmersionBar
+
+
+## app 壳工程
+
+## lib_util 工具类
+
+## lib_main 主要界面功能区
+
--- lib_main/src/main/java/com/example/lib_main/main/MainActivity.kt
+++ lib_main/src/main/java/com/example/lib_main/main/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.lib_main.main
 
 import android.os.Bundle
+import android.widget.ImageView
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.lib_main.R
 import com.example.lib_main.base.ARouteManage
 import com.example.lib_main.base.BaseActivity
+import com.sum.glide.setUrl
 @Route(path=ARouteManage.mainActivity)
 class MainActivity:BaseActivity() {
     override fun initView(savedInstanceState: Bundle?) {
         setContentLayout(R.layout.activity_main)
         setLeftBack()
         setTitle("首页")
+        var img=findViewById<ImageView>(R.id.imgView)
+        img.setUrl("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
     }
 }
