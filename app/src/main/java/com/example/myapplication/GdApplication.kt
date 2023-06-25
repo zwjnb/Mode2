bc576add5543d25cbcc17f832a8f6d13734af882
zwj <742188034@qq.com>
2023-06-25 09:41:26 +0800

    拍照

--- app/src/main/java/com/example/myapplication/GdApplication.kt
+++ app/src/main/java/com/example/myapplication/GdApplication.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.app.Application
 import android.os.Build
+import android.os.Bundle
 import androidx.multidex.BuildConfig
 import androidx.multidex.MultiDexApplication
 import com.alibaba.android.arouter.launcher.ARouter
+import com.example.lib_util.log.LogUtil
+import com.jingewenku.abrahamcaijin.commonutil.ActivityLifecycleCallbacks.SimpleActivityLifecycleCallbacks
 import com.jingewenku.abrahamcaijin.commonutil.AppToastMgr
 import com.sum.framework.helper.SumAppHelper
 
@@ -18,6 +22,33 @@ class GdApplication :MultiDexApplication() {
         ARouter.init(this)
         //toast上下文赋值
         AppToastMgr.init(this)
+        //全局application赋值
         SumAppHelper.init(this,BuildConfig.DEBUG)
+        //获取app退出前后台
+        var life=  object : SimpleActivityLifecycleCallbacks(){
+            override fun onActivityStarted(p0: Activity) {
+            }
+
+            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+            }
+
+            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+            }
+
+            override fun onActivityResumed(p0: Activity) {
+                super.onActivityResumed(p0)
+                LogUtil.e("================>前台")
+            }
+
+            override fun onActivityStopped(p0: Activity) {
+                super.onActivityStopped(p0)
+                LogUtil.e("================>后台")
+            }
+            override fun onActivityDestroyed(p0: Activity) {
+                super.onActivityDestroyed(p0)
+            }
+
+        }
+        SumAppHelper.getApplication().registerActivityLifecycleCallbacks(life)
     }
 }
--- lib_main/src/main/java/com/example/lib_main/main/MainActivity.kt
+++ lib_main/src/main/java/com/example/lib_main/main/MainActivity.kt
@@ -2,29 +2,32 @@ package com.example.lib_main.main
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.view.KeyEvent
 import android.view.LayoutInflater
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.example.lib_main.R
 import com.example.lib_main.base.ARouteManage
 import com.example.lib_main.base.BaseActivity
 import com.example.lib_main.databinding.ActivityMainBinding
 import com.example.lib_util.down.DownloadGO
 import com.example.lib_util.down.DownloadStatus
 import com.example.lib_util.log.LogUtil
-import com.jingewenku.abrahamcaijin.commonutil.ActivityLifecycleCallbacks.SimpleActivityLifecycleCallbacks
+import com.hjq.permissions.Permission
 import com.jingewenku.abrahamcaijin.commonutil.AppExit2Back
-import com.jingewenku.abrahamcaijin.commonutil.AppToastMgr
+import com.luck.picture.lib.basic.PictureSelector
 import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.config.SelectMimeType
 import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnResultCallbackListener
 import com.luck.picture.lib.utils.MediaUtils
+import com.sum.glide.GlideEngine
+import com.sum.glide.MeOnSelectLimitTipsListener
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.*
@@ -41,57 +44,124 @@ class MainActivity:BaseActivity() {
         setLeftBack()
         setTitle("首页")
         img=rootView.imgView
-//      img.setUrl("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
+//        img!!.setUrl("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
         var button=rootView.button
         button.setOnClickListener {
-            download(this)
+//            download(this)
+//            val maxNumPhotosAndVideos = 10
+//            val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
+//            intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, maxNumPhotosAndVideos)
+//            startActivityForResult(intent, 2)
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*" // 相片类
+
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true) //多选
+
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            startActivityForResult(intent, 2)
         }
-      var life=  object :SimpleActivityLifecycleCallbacks(){
-          override fun onActivityStarted(p0: Activity) {
-          }
+       var path= Environment.getExternalStorageDirectory()
+        var absolutePath=   getExternalFilesDir("room")!!.absolutePath
+        LogUtil.e("==================>"+absolutePath)
+        val STORAGE = arrayOf(
+            Permission.READ_MEDIA_IMAGES,
+            Permission.READ_MEDIA_VIDEO,
+            Permission.READ_MEDIA_AUDIO,
+        )
+//      XXPermissions.with(context).permission(STORAGE).request(object:
+//          OnPermissionCallback{
+//          override fun onGranted(permissions: MutableList<String>, allGranted: Boolean) {
+//              if (!allGranted) {
+//                  ToastUtils.showToast(context,"获取部分权限成功，但部分权限未正常授予");
+//                  return;
+//              }
+//              lifecycleScope.launch {
+//                  var file=File(path,"12345.txt")
+//                  if (!file.isDirectory)
+//                      file.createNewFile()
+//                  try {
+//                      var buffer=  BufferedWriter(FileWriter(file,true))
+//                      buffer.write("var file=File(path,\"1234.txt\")\n" +
+//                              "              if (!file.isDirectory)\n" +
+//                              "                  file.createNewFile()\n" +
+//                              "              try {\n" +
+//                              "                  var buffer=  BufferedWriter(FileWriter(file,false))\n" +
+//                              "                  buffer.write(\"1234\")\n" +
+//                              "//                  BufferedReader(FileReader(file,false))\n" +
+//                              "                  LogUtil.e(\"==================>成功\")\n" +
+//                              "              }catch (e:java.lang.Exception){\n" +
+//                              "                  e.printStackTrace()\n" +
+//                              "                  LogUtil.e(\"==================>失败\")\n" +
+//                              "              }")
+////                  BufferedReader(FileReader(file,false))
+//                      LogUtil.e("==================>成功")
+//                  }catch (e:java.lang.Exception){
+//                      e.printStackTrace()
+//                      LogUtil.e("==================>失败")
+//                  }
+//              }
+//              ToastUtils.showToast(context,"获取录音和日历权限成功");
+//          }
+////
+//          override fun onDenied(permissions: MutableList<String>, doNotAskAgain: Boolean) {
+//              if (doNotAskAgain) {
+//                  ToastUtils.showToast(context,"被永久拒绝授权，请手动授予录音和日历权限");
+//                  // 如果是被永久拒绝就跳转到应用权限系统设置页面
+//                  XXPermissions.startPermissionActivity(context, permissions);
+//              } else {
+//                  ToastUtils.showToast(context,"获取录音和日历权限失败");
+//              }
+//          }
+//      })
 
-          override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
-          }
-
-          override fun onActivityCreated(p0: Activity, p1: Bundle?) {
-          }
-
-          override fun onActivityResumed(p0: Activity) {
-              super.onActivityResumed(p0)
-              AppToastMgr.ToastShortCenter("onActivityResumed")
-          }
-
-          override fun onActivityDestroyed(p0: Activity) {
-              super.onActivityDestroyed(p0)
-              AppToastMgr.ToastShortCenter("onActivityDestroyed")
-          }
-
-      }
-
-//        PictureSelector.create(this)
-//            .openGallery(SelectMimeType.ofImage())
-//            .setImageEngine(GlideEngine.createGlideEngine())
+        PictureSelector.create(this)
+            .openGallery(SelectMimeType.ofImage())
+            .setImageEngine(GlideEngine.createGlideEngine())
+            .isDisplayCamera(false)//是否显示相机按钮
 //            .setCropEngine(ImageFileCropEngine(false))//图片剪裁
-//            .setSelectLimitTipsListener( MeOnSelectLimitTipsListener())//拦截自定义提示
-//            .forResult(object : OnResultCallbackListener<LocalMedia?> {
-//                override fun onResult(result: ArrayList<LocalMedia?>?) {
-//                    analyticalSelectResults(result)
-//                }
-//                override fun onCancel() {
-//
-//                }
-//            })
+            .setSelectLimitTipsListener( MeOnSelectLimitTipsListener())//拦截自定义提示
+            .forResult(object : OnResultCallbackListener<LocalMedia?> {
+                override fun onResult(result: ArrayList<LocalMedia?>?) {
+                    analyticalSelectResults(result)
+                }
+                override fun onCancel() {
 
-   lifecycleScope.launch {
-       flow<String> {
-           emit("1")
-       }.flowOn(Dispatchers.IO)
-           .onCompletion {  }
-           .onStart {  }
-           .collect {}
-   }
+                }
+            })
+
+        flow<String> {
+            emit("1")
+        }.flowOn(Dispatchers.IO)
+            .onCompletion {  }
+            .onStart {  }
+            .launchIn(lifecycleScope)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            // 处理错误
+            return
+        }
+        when (requestCode) {
+            1 -> {
+                //单选模式得到图片.
+                val currentUri: Uri = data!!.data!!
+
+                // 在这里处理photo/video URI。返回的应该是provider的路径，需要转成file
+                return
+            }
+            2 -> {
+                // 多选模式得到图片.
+                var i = 0
+                data!!.clipData!!.itemCount
+//                for(i  until data.clipData!!.itemCount){
+//                    var uri:Uri=data.clipData!!.getItemAt(i).uri;
+//                    // 在这里处理每一个photo/video URI。返回的应该是provider的路径，需要转成file
+//                }
+                return;
+            }
+    }}
     override fun onDestroy() {
         super.onDestroy()
 
@@ -127,6 +197,7 @@ class MainActivity:BaseActivity() {
             img!!.setImageURI(Uri.parse(media.cutPath!!))
             else
                 img!!.setImageURI(Uri.parse(media.realPath!!))
+
 
 //            Log.i(MainActivity.TAG, "文件名: " + media.fileName)
 //            Log.i(MainActivity.TAG, "是否压缩:" + media.isCompressed)
